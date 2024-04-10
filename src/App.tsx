9127522d712bc2d6e29a3fb84c2e99ecdf0504ce
Tellemore-J <jeffrey@tellemore.tech>
2024-04-10 16:26:16 -0400

    init project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-screen max-w-screen h-screen bg-gray-800">
+      <header className="w-full bg-white px-5 py-4">
+        <h1>This is my start point</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
